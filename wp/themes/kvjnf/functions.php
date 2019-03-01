c896a7d64c72a422bd9797ca7af1e0a8ec0603c4
Daisuke Akiyama <dai.akiyam@gmail.com>
2019-03-01 22:08:48 +0100

    アイキャッチ画像を表示するコードを追加

--- wp/themes/kvjnf/functions.php
+++ wp/themes/kvjnf/functions.php
@@ -13,3 +13,17 @@ function create_post_type() {
         'rest_base' => 'top',
     ]);
 }
+
+function my_customize_rest_cors() {
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: GET' );
+        header( 'Access-Control-Allow-Headers: X-Requested-With' );
+		header( 'Access-Control-Expose-Headers: Link', false );
+		return $value;
+	} );
+}
+add_action( 'rest_api_init', 'my_customize_rest_cors', 15 );
+
+add_theme_support('post-thumbnails');
